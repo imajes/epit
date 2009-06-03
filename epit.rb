39b2ad99295ecc966584313e50693236c91d058b
James Cox <james@imaj.es>
2009-06-03 15:42:17 +0100

    adding in register user func and basic user modelling

--- epit.rb
+++ epit.rb
@@ -1,15 +1,44 @@
 require 'rubygems'
 require 'sinatra'
 require 'erb'
+require 'memcached'
+
 require 'helpers'
+require 'models/player'
+
 
 set :public, File.dirname(__FILE__) + '/static'
+$cache = Memcached.new("localhost:11211")
 
 get '/' do
   erb :index
 end
 
 get '/new_scan' do
-  @rv = get_reader
+  playa = Player.find(get_reader)
   erb :scan
+end
+
+
+## admin type things
+
+get '/admin' do
+  erb :admin
+end
+
+get '/register_new_user' do
+  @bad_oyster = true if params[:use_oyster]
+  erb :register_user
+end
+
+post '/register_new_user' do
+  begin
+    playa = Player.new(get_reader)
+  rescue NotOysterError
+    redirect "/register_new_user?use_oyster=true"
+  end
+  
+  playa.name = params["name"]
+  playa.save!
+  redirect "/register_new_user"
 end
---
+++ models/player.rb
@@ -0,0 +1,41 @@
+class NotOysterError < Exception; end;
+
+class Player
+  
+  attr_accessor :data, :name, :id
+  
+  def initialize(id)
+    self.id = id.strip
+    check_id
+    # if we don't find 
+    unless find(self.id)
+      self.data = {}
+    end
+      
+  end
+  
+  def check_id
+    if self.id.length != 24
+      raise NotOysterError
+    end
+  end
+  
+  def save!
+    to_write = Marshal.dump(self.data)
+    $cache.set "user_#{self.id}", to_write
+  end
+  
+  
+  private
+  
+  def find(id)
+    begin
+      user = $cache.get "user_#{id}"
+    rescue Memcached::NotFound
+      return false
+    end
+    self.data = Marshal.load(user)
+    return true
+  end
+  
+end
